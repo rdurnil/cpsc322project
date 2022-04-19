fa6d33f3fd66b1007a659e1846cc76655e3bf5b9
Kevin Olbright <kolbright@zagmail.gonzaga.edu>
2022-04-19 22:04:55 +0000

    Added find and count unique items

--- myutils.py
+++ myutils.py
@@ -35,3 +35,34 @@ def bar_chart(x, y, title="", xlabel="", ylabel="", rotation=0):
     plt.xticks(x, rotation=rotation)
     plt.grid()
     plt.show()
+
+def find_unique_items(item_list):
+    """Finds the unique items in a list
+        Args:
+            item_list (list of objs): list of objects to search
+        Returns:
+            a sorted list of the unique items
+    """
+    unique_list = []
+    for _, row in enumerate(item_list):
+        if row not in unique_list:
+            unique_list.append(row)
+    unique_list.sort()
+    return unique_list
+
+def count_unique_items(data_set, unique_items):
+    """Counts the number of unique items in a 1D list
+        Args:
+            data_set (list): the list being searched
+            unique_items (list): the items being searched for
+        Returns:
+            A list of the number of each unique item
+    """
+    count_list = []
+    for i, _ in enumerate(unique_items):
+        num_this_item = 0
+        for j,_ in enumerate(data_set):
+            if unique_items[i] == data_set[j]:
+                num_this_item += 1
+        count_list.append(num_this_item)
+    return count_list
