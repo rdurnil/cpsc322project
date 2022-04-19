ced41c0e3931bf355114604203d7fb7b0c3a9637
Rie <rdurnil@zagmail.gonzaga.edu>
2022-04-19 21:53:27 +0000

    classified ozone

--- myutils.py
+++ myutils.py
@@ -7,25 +7,10 @@ def ozone_assigning(ozone_value):
         Returns:
             A list of the classified values
     """
-    if float(ozone_value) >= 45.0:
-        assigned_number = 10
-    elif float(ozone_value) >= 37.0:
-        assigned_number = 9
-    elif float(ozone_value) >= 31.0:
-        assigned_number = 8
-    elif float(ozone_value) >= 27.0:
-        assigned_number = 7
-    elif float(ozone_value) >= 24.0:
-        assigned_number = 6
-    elif float(ozone_value) >= 20.0:
-        assigned_number = 5
-    elif float(ozone_value) >= 17.0:
-        assigned_number = 4
-    elif float(ozone_value) >= 15.0:
-        assigned_number = 3
-    elif float(ozone_value) > 13.0:
-        assigned_number = 2
+    if float(ozone_value) >= 36.0:
+        assigned_class = "High"
+    elif float(ozone_value) >= 16.0:
+        assigned_class = "Normal"
     else:
-        assigned_number = 1
-
-    return assigned_number
+        assigned_class = "Low"
+    return assigned_class
