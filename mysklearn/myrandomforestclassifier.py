651dad8dbf6df62eb0b5e6506b85aabd7d76e983
Kevin Olbright <kolbright@zagmail.gonzaga.edu>
2022-05-01 17:05:19 +0000

    worked more on the classifier

--- mysklearn/myrandomforestclassifier.py
+++ mysklearn/myrandomforestclassifier.py
@@ -41,13 +41,15 @@ class MyRandomForestClassifier:
         Returns:
             y_predicted(list of numeric vals): The predicted target y values (parallel to X_test)
         """
+        y_predicted = []
         predicted_values = []
         for i, classifier in enumerate(self.classifier_forest):
             predicted_values.append(classifier.predict(X_test))
 
-        for i, _ in enumerate(predicted_values[0]):
-            for j, results in enumerate(predicted_values):
-                pass # this is where we go through each classifiers row and vote on 
-                    # the classifiers vote
+        for i, _ in enumerate(X_test): # this should iterate through the rows
+            all_results = []
+            for j, results in enumerate(predicted_values): # this should iterate through was classifier at the row and add the value to a list
+                all_results.append(predicted_values[j][i]) # this should append the value from the classifier and the row to the holder list
+            
         
         pass #TODO: Fix this
