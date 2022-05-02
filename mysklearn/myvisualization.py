edf396210d8868f9b96dca52482b82015fbc0378
Kevin Olbright <kolbright@zagmail.gonzaga.edu>
2022-05-02 18:14:53 +0000

    more on tech report

--- mysklearn/myvisualization.py
+++ mysklearn/myvisualization.py
@@ -1,11 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter_chart_creation(x, y, x_label='', y_lable='', title=''):
+def scatter_chart_creation(x, y, x_label='', y_label='', title=''):
     plt.figure()
     plt.scatter(x, y, marker="x", s=100, c="purple")
     plt.xlabel(x_label)
-    plt.ylabel(y_lable)
+    plt.ylabel(y_label)
     plt.title(title)
     plt.tight_layout() 
     plt.show()
